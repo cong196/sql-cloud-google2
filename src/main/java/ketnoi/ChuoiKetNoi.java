b604afe151511b8efa0284b641641911fb62abaf
cong196 <vancongct.96@gmail.com>
2017-05-05 08:00:45 +0700

    Update ChuoiKetNoi.java

--- src/main/java/ketnoi/ChuoiKetNoi.java
+++ src/main/java/ketnoi/ChuoiKetNoi.java
@@ -1,17 +1,17 @@
 package ketnoi;
 
 public class ChuoiKetNoi {
-	public String sql="jdbc:mysql://localhost:3306/btdientoan";
+	/*public String sql="jdbc:mysql://localhost:3306/btdientoan";*/
 	public String name="root";
 	
-	public String pass="";
+	//public String pass="";
 	
-	//public String pass="dtdmnhom3";
+	public String pass="dtdmnhom3";
 	
-	/* String sql = String.format(
+	 String sql = String.format(
 		        "jdbc:mysql://google/%s?cloudSqlInstance=%s&"
 		            + "socketFactory=com.google.cloud.sql.mysql.SocketFactory", "dtdmnhom3","btnhom3-sql:us-central1:btdientoannhom3");
-	*/
+	
 	public String getSQL()
 	{
 		return sql;
