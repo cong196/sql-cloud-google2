4362a6c3e97a4b7937ef7c333cb378dc772867da
cong196 <vancongct.96@gmail.com>
2017-05-05 09:58:00 +0700

    Update ChuoiKetNoi.java

--- src/main/java/ketnoi/ChuoiKetNoi.java
+++ src/main/java/ketnoi/ChuoiKetNoi.java
@@ -10,7 +10,7 @@ public class ChuoiKetNoi {
 	
 	 String sql = String.format(
 		        "jdbc:mysql://google/%s?cloudSqlInstance=%s&"
-		            + "socketFactory=com.google.cloud.sql.mysql.SocketFactory", "btdientoannhom3","btnhom3-tag-sql:asia-northeast1:btdientoannhom3");
+		            + "socketFactory=com.google.cloud.sql.mysql.SocketFactory", "btdientoannhom3","btnhom3-sql-tag:us-central1:btdientoannhom3");
 	
 	public String getSQL()
 	{
